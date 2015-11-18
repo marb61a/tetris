128dacd4d7dad3d67baf22ac2cacdad4588fee4a
marb <marb61a@gmail.com>
2015-11-18 11:54:31 +0000

    added key binds to main js

--- js/src/main.js
+++ js/src/main.js
@@ -6,3 +6,29 @@ requirejs.config({
 	}
 });
 
+require(["src/Game"], function(Game){
+	var app = Game.extend({
+		init: function() {
+			canvas.width = 480;
+			canvas.height = 272;
+			canvas.scale = 1;
+
+			content.load("back", "res/back.png");
+			content.load("blocks", "res/blocks.png");
+			content.load("numbers", "res/numbers.png");
+
+			input.bindKey("space", input.Keys.SPACE);
+			input.bindKey("left", [input.Keys.LEFT_ARROW, input.Keys.A]);
+			input.bindKey("up", [input.Keys.UP_ARROW, input.Keys.W]);
+			input.bindKey("right", [input.Keys.RIGHT_ARROW, input.Keys.D]);
+			input.bindKey("down", [input.Keys.DOWN_ARROW, input.Keys.S]);
+
+			this.hasLoad = false;
+		},
+		
+		tick: function(){
+			
+		}
+		
+		)
+});
